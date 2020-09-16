711960005e3a0bfcbd7788b266f03b989ef66303
Tazwar <tazwar9t6@gmail.com>
2020-09-16 10:21:56 +0600

    destination date added

---
+++ src/Data/DestinationData/destinations.js
@@ -0,0 +1,29 @@
+cont destinations = [
+    {
+        id: 1,
+        name: "Cox'sbazar",
+        description: "Coxs Bazar is a town on the southeast coast of Bangladesh. Its known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. ",
+        img: "https://i.ytimg.com/vi/YYgacUGQL1k/maxresdefault.jpg"
+    },
+    {
+        id: 2,
+        name: "Sreemangal",
+        description: "Sreemangal is an Upazila of Moulvibazar District in the Sylhet Division of Bangladesh.",
+        img: "https://cdn.tourradar.com/s3/tour/1500x800/125840_d3be4e42.jpg"
+    },
+    {
+        id: 3,
+        name: "Sundarban",
+        description: "The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Sundarban_Tiger.jpg/1200px-Sundarban_Tiger.jpg"
+    },
+  
+    {
+        id: 4,
+        name: "St. Martin's Island",
+        description: "St. Martin's Island is a small island in the northeastern part of the Bay of Bengal, about 9 km south of the tip of the Cox's Bazar-Teknaf peninsula, and forming the southernmost part of Bangladesh. ",
+        img: "https://www.travelsolutiondesk.com/wp-content/uploads/2019/12/SAINT-MARTIN.jpg"
+    }
+]
+
+export default destinations;
