1a97b9db5fc80e6fc99fb7f495893b9a8e626a09
Tazwar <tazwar9t6@gmail.com>
2020-09-18 15:37:37 +0600

    hotels design added

--- src/Components/hotels/AllHotels.js
+++ src/Components/hotels/AllHotels.js
@@ -8,23 +8,31 @@ const AllHotels = (props) => {
 
   return (
     <>
-      
       {targetPlace.map((hotel) => (
         <>
-          <div className="row">
+          <div className="row p-3">
             <div className="d-flex justify content between px-2">
               <div>
                 <img
-                  className="rounded"
-                  style={{ height: "200px" }}
+                  className="rounded m-3"
+                  style={{ height: "200px", width: "250px" }}
                   src={hotel.image}
                   alt=""
                 />
               </div>
-              <div className="p-3">
+              <div className="p-3 mt-4">
                 <h4>{hotel.name}</h4>
-                <small>Rating: 7(10)</small>
-                <small>Price: ${hotel.price} per night</small>
+                <small>Guests: {hotel.guests}</small> <br />
+                <small>Bed:{hotel.bed}</small> <br />
+                <small>Bath: {hotel.bath}</small> <br />
+                <small>
+                  Rating: <span className="text-info">{hotel.rating}</span> (10)
+                </small>
+                <br />
+                <h5>
+                  Price: <span className="text-success">${hotel.price}</span>{" "}
+                  /night
+                </h5>
               </div>
             </div>
           </div>
